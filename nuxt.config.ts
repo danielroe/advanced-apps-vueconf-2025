f33508723ef697e24fc7d1101874c5b1efd8435c
Daniel Roe <daniel@roe.dev>
2025-05-19 10:34:59 -0400

    feat: add more modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   // https://nitro.build/deploy#compatibility-date
   compatibilityDate: 'latest',
+
   routeRules: {
     '/**': { ssr: false },
     '/about': { prerender: true },
   },
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+  modules: ['@nuxt/fonts', '@nuxt/eslint', '@nuxt/scripts']
 })
